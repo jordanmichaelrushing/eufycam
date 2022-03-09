5e06fc1117fd0baa1b4f7721d18535c549706d84
Jordan Rushing <jordan.rushing@storable.com>
2022-03-09 13:43:29 -0600

    Remove logging

--- lib/eufycam/client.rb
+++ lib/eufycam/client.rb
@@ -19,7 +19,11 @@ module Eufycam
       uri = URI("https://mysecurity.eufylife.com/api/v1/#{path}")
 
       Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-        yield http.request(request(path, body))
+        if block_given?
+          yield http.request(request(path, body))
+        else
+          http.request(request(path, body))
+        end
       end
     end
 
@@ -28,7 +32,6 @@ module Eufycam
 
       Net::HTTP::Post.new(uri).tap do |post|
         post.body = body.to_json
-        p auth_token
         post['x-auth-token'] = auth_token unless auth_token.nil?
       end
     end
@@ -38,53 +41,52 @@ module Eufycam
       body[:verify_code] = @verify_code if verify_code
 
       post('passport/login', body) do |response|
-        p JSON.parse(response.body)
         if JSON.parse(response.body).dig('code') == 26006
           @auth_message = JSON.parse(response.body)['msg']
           @auth_token = nil
+          @verify_code = nil
         else
           @auth_token = JSON.parse(response.body)['data']['auth_token']
           @auth_message = nil
+          @verify_code = nil
           if JSON.parse(response.body).dig('msg') == "need validate code"
             send_verify_code
-            @auth_message = 'Verification code sent to the account email address. Please login again with the verification code.'
+            @auth_message = "Verification code sent to the #{email}. Please login again with the verification code."
           end
         end
       end
     end
 
     def send_verify_code
-      post('/sms/send/verify_code', {"message_type":2}) do |response|
-        p JSON.parse(response.body)
-      end
+      post('/sms/send/verify_code', {"message_type":2}) {|f|}
     end
 
-    def list_devices(auth_token = generate_auth_token)
-      return auth_message if auth_token.nil?
+    def list_devices
+      @auth_token ||= generate_auth_token
+      return false if auth_token.nil?
 
       post('app/get_devs_list') do |response|
-        p JSON.parse(response.body)
         JSON.parse(response.body)['data']
       end
     end
 
     def get_device(device_name:)
       @auth_token ||= generate_auth_token
-      return auth_message if auth_token.nil?
+      return false if auth_token.nil?
 
-      list_devices(auth_token)
+      list_devices
              .detect { |d| d['device_name'] == device_name }
              .slice('station_sn', 'device_sn')
     end
 
     def start_stream(device_name:)
       @auth_token ||= generate_auth_token
-      return auth_message if auth_token.nil?
+      return false if auth_token.nil?
 
-      post(
-        'web/equipment/start_stream',
-        get_device(device_name).slice('device_sn')
-      ) do |response|
+      device = get_device(device_name: device_name)
+      return "Failed to find #{device_name}" if device.blank
+
+      post('web/equipment/start_stream', device) do |response|
         JSON.parse(response.body)['data']
       end
     end
@@ -95,9 +97,12 @@ module Eufycam
 
     def get_event_history(device_name:)
       @auth_token ||= generate_auth_token
-      return auth_message if auth_token.nil?
+      return false if auth_token.nil?
 
-      body = get_device(device_name)
+      device = get_device(device_name: device_name)
+      return "Failed to find #{device_name}" if device.blank
+
+      body = device.slice('device_sn')
       post("event/app/get_all_history_record", body) do |response|
         JSON.parse(response.body)['data']
       end
@@ -115,3 +120,10 @@ module Eufycam
     end
   end
 end
+
+
+eufy = Eufycam::Client.new(email: blah@gmail.com', password: 'mypassword')
+devices = eufy.list_devices
+if devices
+  devices.each do |device|
+    device['device_name'], device['cover_path']
